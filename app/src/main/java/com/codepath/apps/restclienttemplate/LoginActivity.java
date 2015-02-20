cd8cdfb9cefde3001cc88cfe80708d50a69e64ca
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2015-02-19 17:12:45 -0800

    Fancy background image for login screen

--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -4,15 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.codepath.apps.restclienttemplate.models.FlickrPhoto;
+import com.codepath.apps.restclienttemplate.utils.BlurTransform;
 import com.codepath.oauth.OAuthLoginActivity;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends OAuthLoginActivity<FlickrClient> {
+
+    private ImageView ivBackground;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+        ivBackground = (ImageView) findViewById(R.id.ivBackground);
+
+        FlickrClientApp.getRestClient().getOneInterestingPhoto(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    FlickrPhoto fp = new FlickrPhoto(response.getJSONObject("photos").getJSONArray("photo").getJSONObject(0));
+                    Picasso.with(LoginActivity.this)
+                            .load(fp.getUrl())
+//                            .centerCrop()
+                            .transform(new BlurTransform(LoginActivity.this))
+                            .into(ivBackground);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    ivBackground.setImageResource(R.drawable.default_background);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                ivBackground.setImageResource(R.drawable.default_background);
+            }
+        });
 	}
 
 	@Override
