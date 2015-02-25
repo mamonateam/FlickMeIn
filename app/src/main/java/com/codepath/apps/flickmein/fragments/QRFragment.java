ded3757b4e00d84142d18e6c4ef89f1f2f287d82
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2015-02-24 23:22:49 -0800

    this comit is a flipe

--- app/src/main/java/com/codepath/apps/flickmein/fragments/QRFragment.java
+++ app/src/main/java/com/codepath/apps/flickmein/fragments/QRFragment.java
@@ -29,6 +29,7 @@ public class QRFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        album = (AuthorizedAlbum) getArguments().getSerializable("album");
     }
 
     @Override
